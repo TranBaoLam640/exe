9be0ff34eb590f7d531a98b477edabdafc0edbe7
tranbaolam <lamtranbao1234@gmail.com>
2026-09-05 00:48:44 +0700

    fix: fix lost file and test: add AuthApiTest.cs

--- backend/DoRentMe.Api.Tests/Infrastructure/CustomWebApplicationFactory.cs
+++ backend/DoRentMe.Api.Tests/Infrastructure/CustomWebApplicationFactory.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
-
+using DoRentMe.Api.Models;
 namespace DoRentMe.Api.Tests.Infrastructure;
 
 public class CustomWebApplicationFactory
@@ -22,6 +22,44 @@ public class CustomWebApplicationFactory
             {
                 options.UseInMemoryDatabase(_databaseName);
             });
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            using var scope = serviceProvider.CreateScope();
+
+            var dbContext =
+                scope.ServiceProvider
+                    .GetRequiredService<DoRentMeDbContext>();
+
+            dbContext.Database.EnsureCreated();
+
+            // Seed CUSTOMER
+            if (!dbContext.Roles.Any(r => r.Code == "CUSTOMER"))
+            {
+                dbContext.Roles.Add(
+                    new Role
+                    {
+                        Code = "CUSTOMER",
+                        Name = "Customer",
+                        Description = "Customer role"
+                    }
+                );
+            }
+
+            // Seed LENDER
+            if (!dbContext.Roles.Any(r => r.Code == "LENDER"))
+            {
+                dbContext.Roles.Add(
+                    new Role
+                    {
+                        Code = "LENDER",
+                        Name = "Lender",
+                        Description = "Lender role"
+                    }
+                );
+            }
+
+            dbContext.SaveChanges();
         });
     }
 }
